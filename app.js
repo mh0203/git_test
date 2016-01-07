4bf8162eeef033086dba4e298722e37350fd79ac
mh0203 <mh0203@gmail.com>
2016-01-07 18:06:28 +0900

    static html file test

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
+/** get 함수 활용하여 Text 출력하는 Code
 app.get('/', function(req, res) {
   res.send('Hello world!');
 });
+**/
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function(){
   console.log('Server On!');
