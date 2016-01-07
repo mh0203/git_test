d9fed0eae383243e45f9f75a7ee0f4e834678de2
mh0203 <mh0203@gmail.com>
2016-01-07 16:30:44 +0900

    app.js is now coded

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-#changed
+function fizzBuzz() {
+  for (var i = 0; i < 1001; i++) {
+    if(i%3 === 0 && i%5 === 0){
+      console.log("fizzBuzz");
+    } else if (i%3===0) {
+      console.log("fizz");
+    } else if (i%5===0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+   }
+ }
+
+ fizzBuzz();
